6c919ff4959f97c894f39a4143f55b0ee9d913bb
Aleks-28 <aleksgautier@protonmail.com>
2024-03-16 12:00:31 +0100

    final

--- bin/run_model.py
+++ bin/run_model.py
@@ -23,7 +23,7 @@ torch.set_float32_matmul_precision('high')
 datapath = "data/data-challenge-student.pickle"
 batch_size = 64
 lr = 1e-5
-max_epochs = 100
+max_epochs = 50
 num_workers = 16
 
 ###########################################
@@ -56,7 +56,7 @@ def train(batch_size=batch_size, lr=lr, max_epochs=max_epochs, num_workers=num_w
     with mlflow.start_run():
         trainer = pl.Trainer(
             max_epochs=max_epochs,
-            callbacks=[TQDMProgressBar(refresh_rate=20), EarlyStopping(monitor='val_loss', patience=20)],)
+            callbacks=[TQDMProgressBar(refresh_rate=20), EarlyStopping(monitor='val_loss', patience=10)],)
 
         trainer.fit(model, train_loader, valid_loader)
 
--- source/dataset.py
+++ source/dataset.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
+from fairlearn.preprocessing import CorrelationRemover
 
 
 class CustomDataset(Dataset):
@@ -38,13 +39,37 @@ class CustomDataset(Dataset):
 def create_datasets(datapath, test_size=0.2):
     with open(datapath, 'rb') as handle:
         data = pd.read_pickle(handle)
-    X_init = data['X_train']
-    Y_init = data['Y']
-    S_init = data['S_train']
+        X_init = data['X_train']
+        Y_init = data['Y']
+        S_init = data['S_train']
+    # concatenate the sensitive feature to the input features
     scaler = StandardScaler().fit(X_init)
     X_init = scaler.transform(X_init)
-    X_train, X_val, y_train, y_val, S_train, S_val = train_test_split(
-        X_init, Y_init, S_init, test_size=test_size, stratify=Y_init, random_state=42)
+    X_init = pd.DataFrame(X_init, columns=data['X_train'].columns)
+    X_init = pd.concat([X_init, S_init], axis=1)
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_init, Y_init, test_size=0.2, stratify=Y_init, random_state=42)
+
+    columns = [str(i) for i in range(X_train.columns.shape[0]-1)]
+    columns.append('gender')
+    X_train.columns = columns
+    X_val.columns = columns
+    print("Correlation remover...")
+
+    cr = CorrelationRemover(sensitive_feature_ids=["gender"])
+
+    X_cr_train = cr.fit_transform(X_train)
+    X_cr_train = pd.DataFrame(X_cr_train, columns=columns[:-1])
+    X_cr_train.set_index(X_train.index, inplace=True)
+    X_cr_train.loc[:, "gender"] = X_train.loc[:, "gender"]
+
+    X_cr_val = cr.transform(X_val)
+    X_cr_val = pd.DataFrame(X_cr_val, columns=columns[:-1])
+    X_cr_val.set_index(X_val.index, inplace=True)
+    X_cr_val.loc[:, "gender"] = X_val.loc[:, "gender"]
+
     train_dataset = CustomDataset(X_train, y_train, S_train, is_train=True)
     val_dataset = CustomDataset(X_val, y_val, S_val, is_train=False)
+
     return train_dataset, val_dataset, scaler
