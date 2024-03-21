17cc912d3d2c22139123bb8804c9a2d6c440b5e2
Aleks-28 <aleksgautier@protonmail.com>
2024-03-21 19:19:37 +0100

    Submission Commit

--- source/dataset.py
+++ source/dataset.py
@@ -1,30 +1,17 @@
-from torch.utils.data import Dataset
 import torch
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import StandardScaler
-from fairlearn.preprocessing import CorrelationRemover
+
+from source.preprocessing import preprocess_data
+from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
     def __init__(self, X, y, S, is_train=True):
         self.is_train = is_train
         if is_train:
-            S = S.to_numpy().reshape(-1, 1)
-            X = np.concatenate([X, S], axis=1)
-            smote = SMOTE()
-            X, y = smote.fit_resample(X, y)
-            S = X[:, -1]
-            X = X[:, :-1]
             self.X = torch.tensor(X, dtype=torch.float32)
             self.y = torch.tensor(y, dtype=torch.long)
             self.S = torch.from_numpy(S).long()
         else:
-            # convert series to tensor
-
-            print(type(X), type(y), type(S))
             self.X = torch.tensor(X.values, dtype=torch.float32)
             self.y = torch.tensor(y.values, dtype=torch.long)
             self.S = torch.tensor(S.values, dtype=torch.float32)
@@ -33,56 +20,17 @@ class CustomDataset(Dataset):
         return len(self.X)
 
     def __getitem__(self, idx):
-        if self.is_train:
-            return self.X[idx], self.y[idx], self.S[idx]
-        else:
-            return self.X[idx], self.y[idx], self.S[idx]
+        return self.X[idx], self.y[idx], self.S[idx]
 
 
 def create_datasets(datapath, test_size=0.2):
-    with open(datapath, 'rb') as handle:
-        data = pd.read_pickle(handle)
-        X_init = data['X_train']
-        Y_init = data['Y']
-        S_init = data['S_train']
-
-    scaler = StandardScaler().fit(X_init)
-    X_pre = scaler.transform(X_init)
-    columns = data['X_train'].columns
-    X_pre = pd.DataFrame(X_pre, columns=columns)
-    X_pre_reset = X_pre.reset_index(drop=True)
-    S_init_reset = S_init.reset_index(drop=True)
-    X_concat = pd.concat([X_pre_reset, S_init_reset], axis=1)
-    # rename last column to gender_class
-    X_concat.columns = [*X_concat.columns[:-1], 'gender_class']
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_concat, Y_init, test_size=test_size, stratify=Y_init, random_state=42)
-
-    X_S_train = pd.DataFrame(X_train)
-    X_S_train.columns = X_S_train.columns.astype(str)
-
-    X_S_val = pd.DataFrame(X_val)
-    X_S_val.columns = X_S_val.columns.astype(str)
-
-    cr = CorrelationRemover(sensitive_feature_ids=["gender_class"])
-    X_cr_train = cr.fit_transform(X_S_train)
-    X_cr_train = pd.DataFrame(X_cr_train)
-    X_cr_train.set_index(X_S_train.index, inplace=True)
-    X_cr_train.loc[:, "gender_class"] = X_S_train.loc[:, "gender_class"]
-    X_cr_val = cr.transform(X_S_val)
-    X_cr_val = pd.DataFrame(X_cr_val)
-    X_cr_val.set_index(X_S_val.index, inplace=True)
-    X_cr_val.loc[:, "gender_class"] = X_S_val.loc[:, "gender_class"]
-    S_train_cr = X_cr_train['gender_class']
-    S_val_cr = X_cr_val['gender_class']
-
-    X_cr_train.drop(columns='gender_class', inplace=True)
-    X_cr_val.drop(columns='gender_class', inplace=True)
+    X_train, X_val, y_train, y_val, S_train, S_val, scaler = preprocess_data(
+        datapath, test_size=0.2)
 
     train_dataset = CustomDataset(
-        X_cr_train, y_train, S_train_cr, is_train=True)
+        X_train, y_train, S_train, is_train=True)
 
     val_dataset = CustomDataset(
-        X_cr_val, y_val, S_val_cr, is_train=False)
+        X_val, y_val, S_val, is_train=False)
 
     return train_dataset, val_dataset, scaler
--- source/preprocessing.py
+++ source/preprocessing.py
@@ -1,21 +1,86 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler
+from fairlearn.preprocessing import CorrelationRemover
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
-import torch
 
 
-def balance_data(X, Y):
-    sm = SMOTE(random_state=42)
-    X_res, Y_res = sm.fit_resample(X, Y)
-    return X_res, Y_res
+def open_data(datapath):
+    with open(datapath, 'rb') as handle:
+        data = pd.read_pickle(handle)
+        X_init = data['X_train']
+        Y_init = data['Y']
+        S_init = data['S_train']
+        columns = data['X_train'].columns
+        S_init_reset = S_init.reset_index(drop=True)
+    return X_init, Y_init, S_init_reset, columns
 
-def norm(data):
-    data = (data - data.mean()) / data.std()
-    return data
 
-def preprocess_data(X, Y):
-    X_norm = norm(X)
-    X_train, X_val, Y_train, Y_val = train_test_split(X_norm, Y, test_size=0.2, random_state=42)
-    X_res, Y_res = balance_data(X_train, Y_train)
-    inputs = torch.tensor(X_res.values).float()
-    labels = torch.tensor(Y_res.values).long()
-    return inputs, labels, X_val, Y_val
+def split_data(X: pd.DataFrame, Y, S, test_size=0.2):
+    X_concat = pd.concat([X, S], axis=1)
+    # rename the last column to 'gender_class'
+    X_concat.columns = X_concat.columns.astype(str)
+    X_concat.columns.values[-1] = 'gender_class'
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_concat, Y, test_size=test_size, stratify=Y, random_state=42)
+    return X_train, X_val, y_train, y_val
+
+
+def remove_correlation(X_train, X_val):
+    X_S_train = pd.DataFrame(X_train)
+    X_S_train.columns = X_S_train.columns.astype(str)
+
+    X_S_val = pd.DataFrame(X_val)
+    X_S_val.columns = X_S_val.columns.astype(str)
+
+    cr = CorrelationRemover(sensitive_feature_ids=["gender_class"])
+    X_cr_train = cr.fit_transform(X_S_train)
+    X_cr_train = pd.DataFrame(X_cr_train)
+    X_cr_train.set_index(X_S_train.index, inplace=True)
+    X_cr_train.loc[:, "gender_class"] = X_S_train.loc[:, "gender_class"]
+    X_cr_val = cr.transform(X_S_val)
+    X_cr_val = pd.DataFrame(X_cr_val)
+    X_cr_val.set_index(X_S_val.index, inplace=True)
+    X_cr_val.loc[:, "gender_class"] = X_S_val.loc[:, "gender_class"]
+    S_train_cr = X_cr_train['gender_class']
+    S_val_cr = X_cr_val['gender_class']
+
+    X_cr_train.drop(columns='gender_class', inplace=True)
+    X_cr_val.drop(columns='gender_class', inplace=True)
+    return X_cr_train, X_cr_val, S_train_cr, S_val_cr
+
+
+def balance_data(X, y, S):
+    S = S.to_numpy().reshape(-1, 1)
+    X = np.concatenate([X, S], axis=1)
+    smote = SMOTE()
+    X, y = smote.fit_resample(X, y)
+    S = X[:, -1]
+    X = X[:, :-1]
+    return X, y, S
+
+
+def norm(data, columns):
+    scaler = StandardScaler().fit(data)
+    X_pre = scaler.transform(data)
+    X_pre = pd.DataFrame(X_pre, columns=columns)
+    X_pre_reset = X_pre.reset_index(drop=True)
+    return X_pre_reset, scaler
+
+
+def preprocess_data(datapath, test_size=0.2):
+    X_init, Y_init, S_init, columns = open_data(datapath)
+    X_pre, scaler = norm(X_init, columns)
+
+    X_train, X_val, y_train, y_val = split_data(
+        X_pre, Y_init, S_init, test_size)
+
+    X_cr_train, X_cr_val, S_train_cr, S_val_cr = remove_correlation(
+        X_train, X_val)
+
+    X_train, y_train, S_train = balance_data(X_cr_train, y_train, S_train_cr)
+
+    return X_train, X_cr_val, y_train, y_val, S_train, S_val_cr, scaler
